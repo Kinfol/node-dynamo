abf4f03605a1c2775c4450ff01c68d1cd2ee0ceb
KinFol <cristian.vladut@dvla.gov.uk>
2019-10-20 11:39:56 +0100

    dynamo params

--- table-opperations.js
+++ table-opperations.js
@@ -1,16 +1,20 @@
 // initialize sdk
 const AWS = require("aws-sdk");
 // config region
-AWS.config.update({region: 'eu-west-3'});
+AWS.config.update({region: 'eu-west-2'});
 
 // To do Find out how to list tables from all regions
 
+// instantiate dynamoDb API
 const dynamodb = new AWS.DynamoDB();
 
-dynamodb.listTables({}, (err, data)=>{
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(data)
-    }
+// this scenario will output a list of <= 10 dynamoDb tables that begin with 'blah'
+// and will provide the LastEvaluatedTableName
+var params = {
+  ExclusiveStartTableName: 'blah',
+  Limit: '10'
+};
+dynamodb.listTables(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
 });
