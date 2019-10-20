c5de03941a8305182b8dfaa48c49f91c1c8d5dd7
KinFol <cristian.vladut@dvla.gov.uk>
2019-10-20 12:31:29 +0100

    destructure table name to params

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Opperations
     - list tables
+    - describe tables
+    - create
+    - update
+    - delete
--- describe_opperations.js
+++ describe_opperations.js
@@ -9,14 +9,12 @@ AWS.config.update({region: 'eu-west-2'});
 // dynamo object
 const dynamodb = new AWS.DynamoDB;
 
-dynamodb.describeTable(
-    { TableName: 'weatherstation_data'},
-    (err, data) => {
-        if(err) {
-            console.log(err);
-        } else {
-            // the third argument '2' is to prity print the output
-            console.log(JSON.stringify(data, null, 2));
-        }
-})
+var params = {
+    TableName: 'weatherstation_data',
+  };
+
+dynamodb.describeTable(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(JSON.stringify(data, null, 2));           // successful response
+});
 
