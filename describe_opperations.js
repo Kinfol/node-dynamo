2f634c8f1c2e1e0a500b80f4ff378c440b75ae5c
KinFol <cristian.vladut@dvla.gov.uk>
2019-10-21 18:48:28 +0100

    restructure describe function to be used in another function

--- describe_opperations.js
+++ describe_opperations.js
@@ -3,19 +3,21 @@ AWS.config.update({region: 'eu-west-2'});
 const docClient = new AWS.DynamoDB;
 
 async function describeTable() {
-    const params = {
-TableName: 'td_notes_sdk',
+const params = {
+    TableName: 'td_notes_sdk',
 };
-
 let response;
 try {
     response = await docClient.describeTable(params).promise()
-
 } catch (e) {
-
     throw e;
 }
 return response;
 }
 
-describeTable().then(result => console.log(JSON.stringify(result, null, 2)));
+async function blas() {
+    const a = await describeTable();
+    console.log('a')
+    console.log(JSON.stringify(a, null ,2));
+}
+blas();
