027f25568d1aa841c9d6e733fc8e5e16a388dca1
KinFol <cristian.vladut@dvla.gov.uk>
2019-10-21 10:14:17 +0100

    WIP

--- describe_opperations.js
+++ describe_opperations.js
@@ -7,16 +7,47 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-west-2'});
 
 // dynamo object
-const dynamodb = new AWS.DynamoDB;
+const docClient = new AWS.DynamoDB;
 
-var params = {
-    TableName: 'weatherstation_data',
-  };
+
 
 // ToDo:
-// Destructure and chek if table exists before return data
+// Use async
 
-dynamodb.describeTable(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(JSON.stringify(data, null, 2));           // successful response
-});
+// const params = {
+//         TableName: 'weatherstation_test',
+//       };
+
+// docClient.describeTable(params, function(err, data) {
+//   if (err) console.log(err, err.stack); // an error occurred
+//   else     console.log(JSON.stringify(data, null, 2));           // successful response
+// });
+
+async function blah() {
+    const params = {
+    TableName: 'weatherstation_test',
+  };
+  let queryResult;
+        try {
+            queryResult = docClient.describeTable(params).promise();
+            
+        } catch (error) {
+            throw error;
+        }
+        return {
+            results: queryResult,
+        };
+};
+
+
+console.log(blah())
+
+
+
+
+
+// async function blag() {
+//     return await queryPostcodeIndex();
+// }
+// const rs = blag();
+// console.log(rs);
